ddcabeeaed131c1b5e37dea2196cc183a5e88c7b
dev+1-at-029958659886 <miguelofoliveir>
2024-11-26 20:29:50 -0300

    feat:: implemented client module with CRUD functionality
    - Added components for client list and form (create/edit).
    - Integrated ngx-mask for phone number input.
    - Implemented filtering, adding, editing, and deleting clients.
    - Ensured dynamic routing for client creation and editing.

--- src/app/client/form/form.component.ts
+++ src/app/client/form/form.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClientService } from '../client.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Client } from '../client.model';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  clientForm: FormGroup;
+  isEditing = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.clientForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      address: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    const clientId = this.route.snapshot.paramMap.get('id');
+    if (clientId) {
+      this.isEditing = true;
+      this.loadClient(clientId);
+    }
+  }
+
+  loadClient(clientId: string): void {
+    this.clientService.getClientById(clientId).subscribe((client: Client) => {
+      this.clientForm.patchValue(client);
+    });
+  }
+
+  onSave(): void {
+    if (this.clientForm.valid) {
+      const client = this.clientForm.value;
+      if (this.isEditing) {
+        this.clientService
+          .updateClient(this.route.snapshot.paramMap.get('id')!, client)
+          .subscribe(() => this.router.navigate(['/client']));
+      } else {
+        this.clientService
+          .createClient(client)
+          .subscribe(() => this.router.navigate(['/client']));
+      }
+    }
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/client']);
+  }
 }
--- src/app/layouts/logged-in-layout/logged-in-layout-routing.module.ts
+++ src/app/layouts/logged-in-layout/logged-in-layout-routing.module.ts
@@ -31,6 +31,11 @@ const routes: Routes = [
         loadChildren: () =>
           import('../../orders/orders.module').then((m) => m.OrdersModule),
       },
+      {
+        path: 'client',
+        loadChildren: () =>
+          import('../../client/client.module').then((m) => m.ClientModule),
+      },
     ],
   },
 ];
